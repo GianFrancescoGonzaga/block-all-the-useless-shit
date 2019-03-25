8ee1539fd8e2b470dcc21dfd82a04701dbc36c21
Erik Dubois <erik.dubois@gmail.com>
2019-03-25 16:52:24 +0800

    added bash script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -e "127.0.0.1 www.facebook.com" >> /etc/hosts
+echo -e "127.0.0.1 facebook.com" >> /etc/hosts
+
+echo -e "127.0.0.1 www.twitter.com" >> /etc/hosts
+echo -e "127.0.0.1 twitter.com" >> /etc/hosts
+
+echo -e "127.0.0.1 www.instagram.com" >> /etc/hosts
+echo -e "127.0.0.1 instagram.com" >> /etc/hosts
+
+echo -e "127.0.0.1 www.reddit.com" >> /etc/hosts
+echo -e "127.0.0.1 reddit.com" >> /etc/hosts
+
+echo "You are now free!"
+echo "Go out and have fun!"
